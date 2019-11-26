2c7703ba2eaad47ad8e91125de4ec563e53c2272
Jaronce Dutil <jaronce.d@gmail.com>
2019-11-26 11:19:01 -0500

    added routes

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,2 +1,10 @@
 class DishesController < ApplicationController
+  def index
+    @dishes = Dish.all
+    @category = Category.find(params[:id])
+  end
+
+  def show
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show]
+
+  resources :restaurants, only: [:show] do
+    resources :dishes, only: [:index ,:show] do
+      resources :review, only: [:new, :create]
+      resources :vote, only: [:create]
+    end
+  end
 end
