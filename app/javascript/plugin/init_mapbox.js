1eb95e3eb520de6ec86a3738479cd18e2e7702fa
Jaronce Dutil <jaronce.d@gmail.com>
2019-12-02 17:30:00 -0500

    mapbox current location

--- app/javascript/plugin/init_mapbox.js
+++ app/javascript/plugin/init_mapbox.js
@@ -6,15 +6,24 @@ const initMapbox = () => {
   const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 16, duration: 0 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 14, duration: 0 });
   };
+
+  let lat = 45.508888;
+  let lng = -73.561668;
+
+  navigator.geolocation.getCurrentPosition((location) => {
+    lat = location.coords.latitude;
+    lng = location.coords.longitude;
+  })
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10',
-      interactive: false
+      interactive: false,
+      center: [lng, lat]
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
@@ -23,13 +32,22 @@ const initMapbox = () => {
       .addTo(map);
     });
     fitMapToMarkers(map, markers);
-    map.addControl(new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true
-      },
-      trackUserLocation: true
-    }));
+
+      map.addControl(new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true
+        },
+        showUserLocation: true,
+        fitBoundsOptions: {
+          maxZoom:14
+        }
+      }));
   }
+
+  setTimeout(() => {
+    document.querySelector(".mapboxgl-ctrl-icon.mapboxgl-ctrl-geolocate").click()
+
+  }, 100)
 };
 
 export { initMapbox };
