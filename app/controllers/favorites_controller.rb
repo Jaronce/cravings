5805f5e18bbfb1530bb8ba06fb89c280fdcdea62
richard jarram <rjarram@me.com>
2019-12-05 14:28:17 -0500

    ajax done

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,12 +6,21 @@ class FavoritesController < ApplicationController
     @favorite.dish = @dish
 
     @favorite.save
-    redirect_back(fallback_location: root_path)
+
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
+    @dish = @favorite.dish # we need `dish.id` to identify which cards to remove in the DOM.
     @favorite.destroy
-    redirect_back(fallback_location: root_path)
+
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
   end
 end
