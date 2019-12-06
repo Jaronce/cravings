3730e63da45fd2a0f0850a07b8adcf6491fed69f
Jaronce Dutil <jaronce.d@gmail.com>
2019-12-06 14:05:32 -0500

    meta stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :photo ])
+  end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 end
